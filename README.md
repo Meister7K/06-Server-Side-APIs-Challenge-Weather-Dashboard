0d692436906a7bf8abf1b47a89b7f05aa3b5a73e
7K <kmanfinkel17@gmail.com>
2023-04-15 20:40:08 -0500

    added data grab functions"

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 06-Server-Side-APIs-Challenge-Weather-Dashboard
+
+
+## cited 
+- https://developer.mozilla.org/en-US/docs/web/javascript/reference/operators/new
+ - https://stackoverflow.com/questions/66739321/is-fetch-data-from-api-and-get-data-from-api-the-same
+ - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
+ - https://openweathermap.org/forecast5#geo5
--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,7 +1,7 @@
 // ! Global Variables
-const apiKey = '9e15a536f961ad507e941b3cbd627793';
+const apiKey = '499f188a6ab6f2aa9d9a609dac8ea3f2';
 var currentWeather = $('#current-weather');
-var weatherForecast =$('#weather-forecast');
+var weatherForecast = $('#weather-forecast');
 var cityArray; 
 
 // Set Local Storage
@@ -14,13 +14,48 @@ if(localStorage.getItem("weatherSearches")){
 // !Functions
 // Get current Weather /append into html/ 
 function getCurrentWeather(cityName) {
-    var queryURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=imperial`;
-    
+    var queryURL = `api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+    $.get(queryURL).then((response) =>{
+        var todaysDate = new Date(response.dt * 1000);
+        console.log(response);
+        console.log(todaysDate);
+        var weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currentWeather.html(`<h3>${response.name}, ${response.city.country} (${todaysDate.getMonth()}/${todaysDate.getDay()}/${todaysDate.getFullYear()})<img src=${WeatherIcon}></h2>
+        <p>Temp:${response.main.temp} F</p>
+        <p>Humidity:${response.main.humidity}%</p>
+        <p>WindSpeed:${response.wind.speed} m/s</p>
+        <p>UV Index: </p>`) //add uv index response
+    })
+
 }
 
-
 // Get weather forecast /append into html/
+function getWeatherForecast(cityName) {
+    var queryURL = `api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
 
+    $.get(queryURL).then((response) =>{
+        var forecastData = response.list;
+        weatherForecast.empty();
+        $.each(forecastData, (i) =>{
+            var forecastDate = new Date(forecastData[i].dt * 1000);
+            var weatherIcon = `https://openweathermap.org/img/wn/${forecastData[i].weather[0].icon}.png`;
+            weatherForecast.append(`
+            <div class="col-m">
+                <div class="card bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()}/${forecastDate.getDay()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon}>
+                        <p>Temp:${forecastData[i].main.temp} F>/p>
+                        <p>Humidity: ${forecastData[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>`)//ended here 
+        })
+    })
+
+};
 // Get search history buttons /append into html/
 
 // initial calls 
@@ -29,7 +64,7 @@ function getCurrentWeather(cityName) {
 $('#city-search-button').click((e) => {
     e.preventDefault();
     var cityName = $('#city-input').val();
-    // call current weather (cityName)
+    getCurrentWeather(cityName);
     // call weather forecast (cityName)
 })
 
@@ -37,3 +72,173 @@ $('#city-search-button').click((e) => {
 // notes: 
 // - find codes for fahrenheit and percent signs 
 // -date, icon, temp, humidity, wind speed(1day), UV index(1day)
+                      
+
+// {
+//     "cod": "200",
+//     "message": 0,
+//     "cnt": 40,
+//     "list": [
+//       {
+//         "dt": 1647345600,
+//         "main": {
+//           "temp": 287.39,
+//           "feels_like": 286.38,
+//           "temp_min": 286.69,
+//           "temp_max": 287.39,
+//           "pressure": 1021,
+//           "sea_level": 1021,
+//           "grnd_level": 1018,
+//           "humidity": 58,
+//           "temp_kf": 0.7
+//         },
+//         "weather": [
+//           {
+//             "id": 803,
+//             "main": "Clouds",
+//             "description": "broken clouds",
+//             "icon": "04d"
+//           }
+//         ],
+//         "clouds": {
+//           "all": 71
+//         },
+//         "wind": {
+//           "speed": 3.08,
+//           "deg": 128,
+//           "gust": 4.3
+//         },
+//         "visibility": 10000,
+//         "pop": 0,
+//         "sys": {
+//           "pod": "d"
+//         },
+//         "dt_txt": "2022-03-15 12:00:00"
+//       },
+//       {
+//         "dt": 1647356400,
+//         "main": {
+//           "temp": 287.09,
+//           "feels_like": 286.13,
+//           "temp_min": 286.5,
+//           "temp_max": 287.09,
+//           "pressure": 1021,
+//           "sea_level": 1021,
+//           "grnd_level": 1016,
+//           "humidity": 61,
+//           "temp_kf": 0.59
+//         },
+//         "weather": [
+//           {
+//             "id": 803,
+//             "main": "Clouds",
+//             "description": "broken clouds",
+//             "icon": "04d"
+//           }
+//         ],
+//         "clouds": {
+//           "all": 81
+//         },
+//         "wind": {
+//           "speed": 3.28,
+//           "deg": 168,
+//           "gust": 3.96
+//         },
+//         "visibility": 10000,
+//         "pop": 0,
+//         "sys": {
+//           "pod": "d"
+//         },
+//         "dt_txt": "2022-03-15 15:00:00"
+//       },
+//       {
+//         "dt": 1647367200,
+//         "main": {
+//           "temp": 285.44,
+//           "feels_like": 284.6,
+//           "temp_min": 284.47,
+//           "temp_max": 285.44,
+//           "pressure": 1020,
+//           "sea_level": 1020,
+//           "grnd_level": 1016,
+//           "humidity": 72,
+//           "temp_kf": 0.97
+//         },
+//         "weather": [
+//           {
+//             "id": 804,
+//             "main": "Clouds",
+//             "description": "overcast clouds",
+//             "icon": "04d"
+//           }
+//         ],
+//         "clouds": {
+//           "all": 90
+//         },
+//         "wind": {
+//           "speed": 2.7,
+//           "deg": 183,
+//           "gust": 5.59
+//         },
+//         "visibility": 10000,
+//         "pop": 0,
+//         "sys": {
+//           "pod": "d"
+//         },
+//         "dt_txt": "2022-03-15 18:00:00"
+//       },
+//       .....
+//           {
+//         "dt": 1647766800,
+//         "main": {
+//           "temp": 282.42,
+//           "feels_like": 280,
+//           "temp_min": 282.42,
+//           "temp_max": 282.42,
+//           "pressure": 1036,
+//           "sea_level": 1036,
+//           "grnd_level": 1033,
+//           "humidity": 60,
+//           "temp_kf": 0
+//         },
+//         "weather": [
+//           {
+//             "id": 802,
+//             "main": "Clouds",
+//             "description": "scattered clouds",
+//             "icon": "03d"
+//           }
+//         ],
+//         "clouds": {
+//           "all": 39
+//         },
+//         "wind": {
+//           "speed": 4.58,
+//           "deg": 83,
+//           "gust": 8.45
+//         },
+//         "visibility": 10000,
+//         "pop": 0,
+//         "sys": {
+//           "pod": "d"
+//         },
+//         "dt_txt": "2022-03-20 09:00:00"
+//       }
+//     ],
+//     "city": {
+//       "id": 2643743,
+//       "name": "London",
+//       "coord": {
+//         "lat": 51.5085,
+//         "lon": -0.1257
+//       },
+//       "country": "GB",
+//       "population": 1000000,
+//       "timezone": 0,
+//       "sunrise": 1647324902,
+//       "sunset": 1647367441
+//     }
+//   }
+                          
+  
+                      
